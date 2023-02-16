966a3c0aa4961711f5ab595e9cc1f9ff3a0bc6ff
Aadi Poddar <aadipoddarmail@gmail.com>
2023-02-16 16:47:43 +0530

    Projects Setup
    Setup WPF and C++ Engine Project
    Modified Output paths

---
+++ PrimalEditor/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PrimalEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
