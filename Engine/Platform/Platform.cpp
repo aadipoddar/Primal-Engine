76fcaaa42df80d9a5fe2ed32140600e9fd9f6ace
Aadi Poddar <aadipoddarmail@gmail.com>
2023-06-14 22:34:36 +0530

    Fixes
    - Removed Unnecessary consts
    - Define Macros

--- Engine/Platform/Platform.cpp
+++ Engine/Platform/Platform.cpp
@@ -221,16 +221,19 @@ namespace primal::platform {
 		RegisterClassEx(&wc);
 
 		window_info info {};
-		RECT rc { info.client_area }; // content of window except title bar
+		info.client_area.right = (init_info && init_info->width) ? info.client_area.left + init_info->width : info.client_area.right;
+		info.client_area.bottom = (init_info && init_info->height) ? info.client_area.top + init_info->height : info.client_area.bottom;
+
+		RECT rect { info.client_area };
 
 		// Adjust the window for the correct device size
 		AdjustWindowRect(&rc, info.style, FALSE);
 
 		const wchar_t* caption { (init_info && init_info->caption) ? init_info->caption : L"Primal Game" };
-		const s32 left { (init_info && init_info->left) ? init_info->left : info.client_area.left };
-		const s32 top { (init_info && init_info->top) ? init_info->top : info.client_area.top };
-		const s32 width { (init_info && init_info->width) ? init_info->width : rc.right - rc.left };
-		const s32 height { (init_info && init_info->height) ? init_info->height : rc.bottom - rc.top };
+		const s32 left { init_info ? init_info->left : info.top_left.x };
+		const s32 top { init_info ? init_info->top : info.top_left.y };
+		const s32 width { rect.right - rect.left };
+		const s32 height { rect.bottom - rect.top };
 
 		info.style |= parent ? WS_CHILD : WS_OVERLAPPEDWINDOW;
 
@@ -250,7 +253,7 @@ namespace primal::platform {
 		if (info.hwnd)
 		{
 			// Clear any error
-			SetLastError(0);
+			DEBUG_OP(SetLastError(0));
 			// Set the ID and save it in a long pointer
 			const window_id id { add_to_windows(info) };
 			SetWindowLongPtr(info.hwnd, GWLP_USERDATA, (LONG_PTR)id);
@@ -274,7 +277,7 @@ namespace primal::platform {
 		remove_from_windows(id);
 	}
 
-#elif
+#else
 #error "Must Implement at least one platform"
 
 #endif // _WIN64
@@ -304,7 +307,7 @@ namespace primal::platform {
 		set_window_caption(_id, caption);
 	}
 
-	const math::u32v4 window::size() const
+	math::u32v4 window::size() const
 	{
 		assert(is_valid());
 		return get_window_size(_id);
@@ -316,13 +319,13 @@ namespace primal::platform {
 		resize_window(_id, width, height);
 	}
 
-	const u32 window::width() const
+	u32 window::width() const
 	{
 		math::u32v4 s{ size() };
 		return s.z - s.x;
 	}
 
-	const u32 window::height() const
+	u32 window::height() const
 	{
 		math::u32v4 s{ size() };
 		return s.w - s.y;
