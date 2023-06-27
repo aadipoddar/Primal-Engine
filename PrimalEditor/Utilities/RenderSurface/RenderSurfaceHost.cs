ba2b2e14b30f4bb6ee975dfb42d45fc364f6136f
Aadi Poddar <aadipoddarmail@gmail.com>
2023-06-27 14:58:54 +0530

    Improve Window Resizing

--- PrimalEditor/Utilities/RenderSurface/RenderSurfaceHost.cs
+++ PrimalEditor/Utilities/RenderSurface/RenderSurfaceHost.cs
@@ -9,6 +9,7 @@ namespace PrimalEditor.Utilities
 {
     class RenderSurfaceHost : HwndHost
     {
+        private readonly int VK_LBUTTON = 0x01;
         private readonly int _width = 800;
         private readonly int _height = 600;
 
@@ -16,16 +17,14 @@ namespace PrimalEditor.Utilities
 
         private DelayEventTimer _resizeTimer;
 
-        public int SurfaceId { get; private set; } = ID.INVALID_ID;
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vkey);
 
-        public void Resize()
-        {
-            _resizeTimer.Trigger();
-        }
+        public int SurfaceId { get; private set; } = ID.INVALID_ID;
 
         private void Resize(object sender, DelayEventTimerArgs e)
         {
-            e.RepeatEvent = Mouse.LeftButton == MouseButtonState.Pressed;
+            e.RepeatEvent = GetAsyncKeyState(VK_LBUTTON) < 0;
             if (!e.RepeatEvent)
             {
                 EngineAPI.ResizeRenderSurface(SurfaceId);
@@ -38,6 +37,7 @@ namespace PrimalEditor.Utilities
             _height = (int)height;
             _resizeTimer = new DelayEventTimer(TimeSpan.FromMilliseconds(250.0));
             _resizeTimer.Triggered += Resize;
+            SizeChanged += (s, e) => _resizeTimer.Trigger();
         }
 
         protected override HandleRef BuildWindowCore(HandleRef hwndParent)
