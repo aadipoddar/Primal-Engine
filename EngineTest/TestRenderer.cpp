5189b6f96349037ca00d63e574f5a545c15a2009
Aadi Poddar <aadipoddarmail@gmail.com>
2024-04-14 00:24:27 +0530

    Synchronizing CPU and GPU

--- EngineTest/TestRenderer.cpp
+++ EngineTest/TestRenderer.cpp
@@ -1,9 +1,9 @@
-#ifdef TEST_RENDERER
-
 #include "..\Platform\PlatformTypes.h"
 #include "..\Platform\Platform.h"
 #include "..\Graphics\Renderer.h"
 #include "TestRenderer.h"
+
+#ifdef TEST_RENDERER
 
 using namespace primal;
 
@@ -78,6 +78,7 @@ bool engine_test::initialize()
 void engine_test::run()
 {
 	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	graphics::render();
 }
 
 void engine_test::shutdown()
